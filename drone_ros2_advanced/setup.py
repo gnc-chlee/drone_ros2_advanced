7437c402779f77adca7846da072c8b5c525c22d5
Choonghyun Lee <chungh6577@gmail.com>
2026-09-10 05:37:42 +0900

    Add config/triangle.yaml for the week 3 YAML demo; install all config/*.yaml

--- drone_ros2_advanced/setup.py
+++ drone_ros2_advanced/setup.py
@@ -1,3 +1,4 @@
+from glob import glob
 from setuptools import find_packages, setup
 
 package_name = 'drone_ros2_advanced'
@@ -10,7 +11,7 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        ('share/' + package_name + '/config', ['config/waypoints.yaml']),
+        ('share/' + package_name + '/config', glob('config/*.yaml')),   # 미션 파일 전부 설치
     ],
     install_requires=['setuptools'],
     zip_safe=True,
